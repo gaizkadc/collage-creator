6e91bb8d699de07582e6fca7bfdcedb2305293fd
Gaizka Díaz Cerrillo <gaizka@ordenata.fohome>
2021-05-23 20:26:19 +0200

    some logging added

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const ig = require('./igUtils');
 
 // MAIN
 
+console.log('getting image properties...');
 const imageProperties = iu.getImageProperties();
-const palette = iu.createPalette(imageProperties.grayscale);
 
+console.log('creating palette...');
+const palette = iu.createPalette(imageProperties.grayscale);
+console.log('palette: ' + palette);
+
+console.log('drawing image...');
 du.drawImage(imageProperties, palette);
 
+console.log('posting to instagram...');
 ig.postToIg(palette);
